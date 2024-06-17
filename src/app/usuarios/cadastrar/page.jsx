97854fd9486ccb051f5fe7704f9c019288903690
EMANUEL PEDRO ALVES BOMFIM ROSA <emanuel.rosa01>
2024-06-17 11:38:11 -0300

    tcc 17/06

--- src/app/usuarios/cadastrar/page.jsx
+++ src/app/usuarios/cadastrar/page.jsx
@@ -1,9 +1,74 @@
-export default function CadUsu() {
-  return (
-    <div>
-      <h1>Cadastrar usuários</h1>
-    </div>  
+'use client'
 
+import React, { useState } from 'react';
+import styles from './page.module.css';
+
+const CadUsu = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Nome de usuário:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <br />
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <br />
+        <label htmlFor="password">Senha:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <br />
+        <label htmlFor="confirmPassword">Confirmar Senha:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <br />
+        <button type="submit" className={styles.button}>
+          Cadastrar
+        </button>
+      </form>
+    </div>
   );
-}
-    
+};
+
+export default CadUsu;
